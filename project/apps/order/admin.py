d0850904bdc8896cb277038840c176be30476b30
Milad Faraji <faraji.milad@gmail.com>
2019-08-10 02:25:34 +0430

    add list_filter for order amdin

--- project/apps/order/admin.py
+++ project/apps/order/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from .models import Order
+from .models import Order, OrderProductFeature
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('user', 'price')
+    list_display = ('id', 'user', 'price', 'created_at')
+    list_filter = ('status', )
+
+
+class OrderProductFeatureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_feature', 'number', 'created_at')
+
+    # def get_product(self):
+    #     return self.pro
 
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProductFeature, OrderProductFeatureAdmin)
+
